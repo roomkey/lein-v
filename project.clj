74f21a8040713b14eb869c138d66b527766263b9
David Sison <sisonfam@gmail.com>
2012-11-13 18:53:25 -0500

    Remove hard-coded creds + idiomatic lein 2 project

--- project.clj
+++ project.clj
@@ -2,13 +2,10 @@
   :description "A Leiningen plugin to reflect on the version of a project"
   :url "https://github.com/g1nn13/lein-v"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[midje "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :plugins [[s3-wagon-private "1.1.2"]]
-  :repositories [["releases"
-                  {:url "s3p://rk-maven/releases/"
-                   :passphrase "tfoUGPp0xWYtZS1jdhRKWD+o18P065h0QrpbaVae"
-                   :username "AKIAJUGQVZJ43WJOFM3Q"}]
-                 ["snapshots" {:url "s3p://rk-maven/snapshots/"}]]
+  :repositories [["releases" {:url "s3p://rk-maven/releases/" :username :env :passphrase :env}]
+                 ["snapshots" {:url "s3p://rk-maven/snapshots/" :username :env :passphrase :env}]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
