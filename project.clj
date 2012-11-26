961c58bf504b126edb14d70303f66fd1022fd7b0
Chris Hapgood <cch1@hapgoods.com>
2012-11-26 10:04:50 -0500

    Add roomkey repo for deploys and bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject com.roomkey/lein-v "3.0.0"
+(defproject com.roomkey/lein-v "3.1.0"
   :description "A Leiningen plugin to reflect on the version of a project"
   :url "https://github.com/g1nn13/lein-v"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["releases" {:url "s3p://rk-maven/releases/"}]]
   :eval-in-leiningen true)
