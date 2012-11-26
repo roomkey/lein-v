cd73cb4b980737f4b75fc824cb72ae48133afc11
Chris Hapgood <cch1@hapgoods.com>
2012-11-26 11:57:47 -0500

    Tweak project.clj for better leiningen v2 experience

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject com.roomkey/lein-v "3.1.0"
   :description "A Leiningen plugin to reflect on the version of a project"
   :url "https://github.com/g1nn13/lein-v"
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :license {:name "Eclipse Public License"
