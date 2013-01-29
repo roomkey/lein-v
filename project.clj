38dc2f15a5a13946239735a0bc28279f89e9d17f
Chris Hapgood <cch1@hapgoods.com>
2013-01-29 14:14:01 -0500

    Hardcode version in project.clj to allow git consumers to deploy quickly

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject com.roomkey/lein-v :lein-v
+(defproject com.roomkey/lein-v "3.3.5"
   :description "A Leiningen plugin to reflect on the SCM workspace of a project"
   :url "https://github.com/g1nn13/lein-v"
   :plugins [[s3-wagon-private "1.1.2"]]
-  :middleware [leiningen.v/version-from-scm]
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
