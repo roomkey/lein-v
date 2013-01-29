52104bccbacf39f3d53507dfac38433499714f56
Chris Hapgood <cch1@hapgoods.com>
2013-01-29 14:36:19 -0500

    Tighten requirements for automatically managing project version

--- src/lein_v/plugin.clj
+++ src/lein_v/plugin.clj
@@ -4,7 +4,13 @@
 (defn hooks []
   (deploy-when-anchored))
 
+(defn- select-versioner [version]
+  (if (or (= :lein-v version) (and (string? version) (or (empty? version) (re-find #"lein" version))))
+    (do (when-not (= :lein-v version)
+          (println "WARNING: Future versions of lein-v will not manage this project's version automatically.  Set version string in project.clj to :lein-v to ensure future compatibility."))
+        version-from-scm)
+    identity))
+
 (defn middleware [{version :version :as project}]
-  (when-not (= :lein-v version)
-    (println "WARNING: Future versions of lein-v will not manage this project's version automatically.  Set version string in project.clj to :lein-v to ensure future compatibility."))
-  (add-workspace-data (version-from-scm project)))
+  (let [versioner (select-versioner version)]
+    (add-workspace-data (versioner project))))
