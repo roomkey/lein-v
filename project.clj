7a09702a93008feafdacdfae55dc6e52e3d29f14
Chris Hapgood <cch1@hapgoods.com>
2013-08-06 14:54:03 -0400

    Bump clojure dependency to latest
    Signed-off-by: Chris Hapgood <cch1@hapgoods.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/g1nn13/lein-v"
   :plugins [[s3-wagon-private "1.1.2"]]
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
