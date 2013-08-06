43c8742675265b8d804282ccdd6d257488cf94cc
Chris Hapgood <cch1@hapgoods.com>
2013-08-06 14:54:46 -0400

    Make lein-v use lein-v for versioning
    Signed-off-by: Chris Hapgood <cch1@hapgoods.com>

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject com.roomkey/lein-v "3.3.5"
+(defproject com.roomkey/lein-v :lein-v
   :description "A Leiningen plugin to reflect on the SCM workspace of a project"
   :url "https://github.com/g1nn13/lein-v"
   :plugins [[s3-wagon-private "1.1.2"]]
   :min-lein-version "2.0.0"
+  :middleware [leiningen.v/version-from-scm]
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :license {:name "Eclipse Public License"
