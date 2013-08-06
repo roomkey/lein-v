1abeb2ac6970bb8f2c74403e5b079eae92596cde
Chris Hapgood <cch1@hapgoods.com>
2013-08-06 14:38:45 -0400

    Remove deprecated auto-management mode

--- src/lein_v/plugin.clj
+++ src/lein_v/plugin.clj
@@ -5,11 +5,11 @@
   (deploy-when-anchored))
 
 (defn- select-versioner [version]
-  (if (or (= :lein-v version) (and (string? version) (or (empty? version) (re-find #"lein" version))))
-    (do (when-not (= :lein-v version)
-          (println "WARNING: Future versions of lein-v will not manage this project's version automatically.  Set version string in project.clj to :lein-v to ensure future compatibility."))
-        version-from-scm)
-    identity))
+  (if (= :lein-v version)
+    version-from-scm
+    (do (when (and (string? version) (or (empty? version) (re-find #"lein" version)))
+          (println "WARNING: lein-v is not managing this project's version.  Set version in project.clj to :lein-v to trigger automatic lein-v management"))
+        identity)))
 
 (defn middleware [{version :version :as project}]
   (let [versioner (select-versioner version)]
