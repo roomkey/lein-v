126517afabac6186870938a9b62ef6387c3f80ea
Vincent Rivellino <vrivellino@roomkey.com>
2014-10-03 19:22:11 +0000

    Updated github URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.roomkey/lein-v :lein-v
   :description "A Leiningen plugin to reflect on the SCM workspace of a project"
-  :url "https://github.com/g1nn13/lein-v"
+  :url "https://github.com/roomkey/lein-v"
   :plugins [[s3-wagon-private "1.1.2"]]
   :min-lein-version "2.0.0"
   :middleware [leiningen.v/version-from-scm]
