50dcfdb30645d4d5ddfb726d8adec80cb7f259e0
Chris Hapgood <cch1@hapgoods.com>
2016-07-19 09:21:27 -0400

    Add tmp directory and java.io.tmpdir property

--- project.clj
+++ project.clj
@@ -19,6 +19,7 @@
                               :exclude-namespaces [:test-paths]}}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ["-Djava.io.tmpdir=./tmp"]
   :repositories [["rk-public" {:url "http://rk-maven-public.s3-website-us-east-1.amazonaws.com/releases/"}]
                  ["releases" {:url "s3://rk-maven-public/releases/"}]
                  ["rk-private" {:url "s3://rk-maven/releases/"}]]
