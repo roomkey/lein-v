6de1bd624a34d63ae03d55865f8c6c04e914e695
Chris Hapgood <cch1@hapgoods.com>
2016-07-18 22:40:54 -0400

    Add more integration tests with bundled repos

--- test/integration/lein_v/plugin.clj
+++ test/integration/lein_v/plugin.clj
@@ -18,6 +18,10 @@
 
 (defn dirty! [] (sh "echo \"Hello\" >> x && git add x"))
 
+;; simple.repo: (do (init!) (commit!) (tag "v1.2.3"))
+;; Create a bundle with: `git bundle create ../simple.repo --all`
+(defn clone! [bundle] (sh (format "git clone %s . -b master" (-> bundle io/resource io/as-file str))))
+
 (background
  (around :facts (let [tmpdir (Files/createTempDirectory
                               (.toPath (io/as-file (io/resource "tmp-git")))
@@ -40,27 +44,33 @@
   => (contains {:version #"0.0.0-1-0x[0-9a-z]{4}"
                :manifest (contains {"Implementation-Version" #"0.0.0-1-0x[0-9a-z]{4}"})}))
 
+(fact "Commit with zero commit distance yields simple version"
+  (against-background (before :facts (do (clone! "test-2.repo"))))
+  (middleware $mproject)
+  => (contains {:version "2.3.4-alpha4"
+               :manifest (contains {"Implementation-Version" "2.3.4-alpha4"})}))
+
 (fact "Commit with positive commit distance yields complete version"
-  (against-background (before :facts (do (init!) (commit!) (tag! "v2.3.4-alpha4") (commit!))))
+  (against-background (before :facts (do (clone! "test-2.repo") (commit!))))
   (middleware $mproject)
   => (contains {:version #"2.3.4-alpha4-1-0x[0-9a-f]{4,}"
                :manifest (contains {"Implementation-Version" #"2.3.4-alpha4-1-0x[0-9a-f]{4,}"})}))
 
 (fact "Dirty repo is marked in version"
-  (against-background (before :facts (do (init!) (commit!) (tag! "v2.3.4-alpha4") (commit!) (dirty!))))
+  (against-background (before :facts (do (clone! "test-2.repo") (commit!) (dirty!))))
   (middleware $mproject)
   => (contains {:version #"2.3.4-alpha4-1-0x[0-9a-f]{4,}-DIRTY"
                :manifest (contains {"Implementation-Version" #"2.3.4-alpha4-1-0x[0-9a-f]{4,}-DIRTY"})}))
 
 (fact "Workspace data is injected"
-  (against-background (before :facts (do (init!) (commit!) (tag! "v2.3.4-alpha4") (commit!) (dirty!))))
+  (against-background (before :facts (do (clone! "test-2.repo") (commit!) (dirty!))))
   (middleware $mproject)
   => (contains {:workspace (just {:describe string? :status map?})
                :manifest (contains {"Workspace-Description" #"v2.3.4-alpha4-1-g[0-9a-f]{4,}-DIRTY"
-                                    "Workspace-Tracking-Status" "## master"
+                                    "Workspace-Tracking-Status" #"## master.*"
                                     "Workspace-File-Status" "A  x"})}))
 
 (fact "deploy-when-anchored hook works"
-  (against-background (before :facts (do (init!) (commit!) (tag! "v2.3.4-alpha4") (commit!) (dirty!))))
+  (against-background (before :facts (do (clone! "test-2.repo") (commit!) (dirty!))))
   (hooks) => anything
   (leiningen.deploy/deploy (middleware $mproject)) => nil)
