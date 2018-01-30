1492101f4f1a1cb017d4e136d8e5967e147dcd31
jeremiegrodziski <jeremie.github@grodziski.com>
2018-01-30 15:29:21 +0000

    add test for version.edn cache file

--- test/unit/leiningen/v/file.clj
+++ test/unit/leiningen/v/file.clj
@@ -9,7 +9,19 @@
   (and (= "..version.." (eval 'version/version))
        (= "..describe.." (eval 'version/raw-version))))
 
+
+(defchecker valid-version-data-structure
+  [actual]
+  (let [version-edn (load-string actual)]
+    (and (= "..version.." (:version version-edn))
+         (= "..describe.." (:raw-version version-edn)))))
+
 (fact "Version source code is cached"
   (cache ..path.. ..version.. ..describe..) => anything
   (provided
     (clojure.core/spit ..path.. valid-version-source) => ..ignored..))
+
+(fact "Version data structure is cached"
+      (cache-edn ..path.. ..version.. ..describe..) => anything
+      (provided
+       (clojure.core/spit ..path.. valid-version-data-structure) => ..ignored..))
